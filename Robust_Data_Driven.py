f589330128238b3c89b1b01df17dbaf4b25fdaa6
jailsonbina2018 <45823632+jailsonbina2018@users.noreply.github.com>
2024-06-17 08:14:01 -0400

    Data-driven  Control FIT

--- Robust_Data_Driven.py
+++ Robust_Data_Driven.py
@@ -17,13 +17,13 @@ Btr = np.array([[0.1, -0.3],
 
 Bw = np.identity(3)
 
-C = np.identity(3)
+Ctr = np.identity(3)
 
-D = 0
+Dtr = 0
 Dw = 0
 
-L = 3  # Número de linhas.
-N = 20  # Número de amostras para cada estado T >= (m + 1)n + m
+Ln = 3  # Número de linhas.
+Nc = 20  # Número de amostras para cada estado T >= (m + 1)n + m
 T_s = 0.1  # Tempo de amostragem
 
 n = 3
@@ -31,80 +31,87 @@ m = 2
 t = 1
 
 # Estado inicial aleatória
-x0 = np.random.rand(L, 1)
+x0 = np.random.rand(Ln, 1)
 u0 = np.random.rand(m, 1) - 1  # Entrada u(k) uniformemente de [-1, 1]
 w_medin = 0.02
 w0 = np.dot(w_medin, np.random.rand(n, 1))
 
 # Entrada aleatória
-u = np.random.rand(m, N) - 1  # Entrada u(k) uniformemente de [-1, 1]
-x = np.zeros((3, N))
-w = np.dot(w_medin, np.random.rand(n, N))
+u = np.random.rand(m, Nc) - 1  # Entrada u(k) uniformemente de [-1, 1]
+x = np.zeros((3, Nc))
+w = np.dot(w_medin, np.random.rand(n, Nc))
 
 for j in range(n):
     x[j, 0] = np.dot(Atr[j, :], x0).item() + np.dot(Bw[j, :], w0).item() + np.dot(Btr[j, :], u0).item()
 
 # Representação em malha aberta baseada em dados
-for k in range(N-1):
+for k in range(Nc-1):
     x[:, k+1] = np.dot(Atr, x[:, k]) + np.dot(Bw, w[:, k]) + np.dot(Btr, u[:, k])
 
 
-X0 = np.hstack((x0, x[:, 1:N-1]))  # X
-X1 = np.hstack((x[:, 1:N]))  # X+
+Xs0 = np.hstack((x0, x[:, 1:Nc]))  # X
+Xds1 = np.hstack((x[:, 1:2], x[:, 1:Nc]))  # X+
 
-print(f'X0 = {np.shape(X0)}')
-print(f'X1 = {np.shape(X1)}')
+# print(f'X0 = {X0}')
+# print(f'X0 = {np.shape(X0)}')
 
-"""
+# print(f'X1 = {X1}')
+# print(f'X1 = {np.shape(X1)}')
+
+
 # Configurações do cvxpy
 gm = 2.4
 
-p = cp.Variable()
-
-X0 = cp.Parameter((n, N), value=X0)
-X1 = cp.Parameter((n, N), value=X1)
+X0 = cp.Parameter((n, Nc), value=Xs0)
+X1 = cp.Parameter((n, Nc), value=Xds1)
 
 Y = cp.Variable(shape=(m, m), symmetric=True)
-M = cp.Variable(shape=(N, n), symmetric=False)
+M = cp.Variable(shape=(Nc, n), symmetric=False)
 
 Q = - np.dot(np.square(gm), np.identity(n))
 S = 0
 R = np.identity(n)
 
-Qw = - np.identity(n)
-Sw = np.zeros((n, n))
-Rw = np.dot(np.square(w_medin), np.identity(N))
+QW = - cp.Parameter((n, n), value=np.identity(n))
+# SW = cp.Parameter((n, Nc), value=np.zeros(n, Nc))
+SW = cp.Parameter((n, Nc), value=np.zeros((n, Nc)))
+RW = cp.Parameter((Nc, Nc), value=np.dot(np.square(w_medin), np.identity(Nc)))
+
+print(np.shape(RW.value))
+
+BW = cp.Parameter((n, n), value=Bw)
 
 constraints = []
 
 H11 = -Y
-H12 = -M.T @ Sw.T
+H12 = -M.T @ SW.T
 H13 = M.T @ X1.T
 H14 = M.T
 
-H21 = -Sw @ M
-H22 = Qw
-H23 = Bw.T
+H21 = -SW @ M
+H22 = QW
+H23 = BW.T
 H24 = 0
 
 H31 = X1 @ M
-H32 = Bw
+H32 = BW
 H33 = -Y
 H34 = 0
 
 H41 = M
 H42 = 0
 H43 = 0
-H44 = - np.linalg.inv(Rw)
+H44 = - np.linalg.inv(RW)
 
 
 H = cp.bmat([
-    [H11, H12, H13],
-    [H21, H22, H23],
-    [H31, H32, H33]])
+    [H11, H12, H13, H14],
+    [H21, H22, H23, H24],
+    [H31, H32, H33, H34],
+    [H41, H42, H43, H44]])
 
 
-obj = cp.Minimize(p)
+obj = cp.Minimize()
 constraints += [Y >> 0]
 constraints += [M >> 0]
 
@@ -115,7 +122,7 @@ prob.solve(solver=cp.MOSEK, verbose=False)
 print(Y.value)
 print(M.value)
 
-"""
+
 
 # Lei de controle
 
